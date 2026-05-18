df691e717ffa5da0e52e40f73dfc72653d43ecea
Sue Raisianzadeh <raisianz@gmail.com>
2026-05-19 02:15:04 +1200

    edit login-register

--- server/DTOs/LoginResponseDto.cs
+++ server/DTOs/LoginResponseDto.cs
@@ -3,10 +3,12 @@ namespace server.DTOs
     public class LoginResponseDto
     {
         public string Message { get; set; } = "";
+        public string UserName { get; set; } = "";
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public string FullName { get; set; } = "";
         public string Role { get; set; } = "";
+        public string Village { get; set; } = "";
         public string ProfileImageUrl { get; set; } = "";
     }
 }
--- server/DTOs/RegisterRequestDto.cs
+++ server/DTOs/RegisterRequestDto.cs
@@ -2,11 +2,13 @@ namespace server.DTOs
 {
     public class RegisterRequestDto
     {
+        public string UserName { get; set; } = "";
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public string Email { get; set; } = "";
         public string Password { get; set; } = "";
         public string Role { get; set; } = "";
+        public string Village { get; set; } = "";
         public string ProfileImageUrl { get; set; } = "";
     }
 }
