2c559f15dcd32dc2efda41c153521f6e8547952f
Sue Raisianzadeh <raisianz@gmail.com>
2026-06-10 00:31:13 +1200

    orders

--- server/Program.cs
+++ server/Program.cs
@@ -29,4 +29,12 @@ app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
+}
+
+
 app.Run();
